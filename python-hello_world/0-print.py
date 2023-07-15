2d6b677ba807f7eff24538d1a87f09e143372660
Natnael Girma <ngirma700@gmail.com>
2023-07-15 14:11:12 +0300

    My first commit

--- python-hello_world/0-print.py
+++ python-hello_world/0-print.py
@@ -1,6 +1,1 @@
-<<<<<<< HEAD
-print(" \"Programming is like building a multilingual puzzle")
-
-=======
-print('\"Programming is like building a multilingual puzzle')
->>>>>>> d36bf1f4bfe20a2122412eacfaa6911827f6d9af
+print(' \"Programming is like building a multilingual puzzle')

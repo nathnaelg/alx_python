e39e0620775f059ff3c7cec620bcd61d430a130f
Natnael Girma <ngirma700@gmail.com>
2023-07-15 18:39:20 +0300

    My first commit

---
+++ python-hello_world/3-last_digit.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+last_digit = abs(number) % 10
+
+if number < 0:
+    last_digit = last_digit * -1
+message = "Last digit of {} is {} ".format(number, last_digit)
+if last_digit > 5:
+    message += "and is greater than 5"
+elif last_digit == 0:
+    message += "and is 0"
+else:
+    message += "and is less than 6 and not 0"
+print(message)

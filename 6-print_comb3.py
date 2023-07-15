e6087609bb0825781e0f3008a3497e3864b04c9a
Natnael Girma <ngirma700@gmail.com>
2023-07-15 20:26:42 +0300

    My first commit

--- 6-print_comb3.py
+++ 6-print_comb3.py
@@ -1,18 +1,4 @@
-def validate_password(password):
-    password = str(password)
-    if len(str(password)) < 8:
-        return False
-    for i in range(len(password)):
-        if password[i] == password[i].upper() and password[i] != password[i].lower():
-            break
-        if password[i] == password[len(password)-1]:
-            return False
-    for i in range(len(password)):
-        if password[i] == password[i].lower() and password[i] != password[i].upper():
-            break
-        if str(i) == password[len(password)-1]:
-            return False
-    for i in range(len(password)):
-        if password[i].isspace():
-            return False
-    return True
+#!/usr/bin/python3
+for i in range(90):
+    print(f'{i:02}', end=', ')
+print()

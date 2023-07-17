fdcc0b3ae581ee795c624f88f786c3b5aee29ccc
Natnael Girma <ngirma700@gmail.com>
2023-07-17 21:27:22 +0300

    My first commit

--- python-functions/1-power.py
+++ python-functions/1-power.py
@@ -1,8 +1,16 @@
-#!/usr/bin/env python3
-pow = __import__('1-power').pow
-
-print(pow(2, 2))
-print(pow(98, 2))
-print(pow(98, 0))
-print(pow(100, -2))
-print(pow(-4, 5))
+def pow(a, b):
+    # Initialize result to
+    result = 1
+    # If b is negative, invert a and make b positive
+    if b < 0:
+        a = 1/a
+        b = -b
+    # Compute a to the power of b using repeated multiplication
+    while b > 0:
+        # If b is odd, multiply result by a
+        if b % 2 == 1:
+            result *= a
+            # Square a and halve b
+        a *= a
+        b //= 2
+    return result

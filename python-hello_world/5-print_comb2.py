5130d7e46908329b98c85b438c2272af1e52a36e
Natnael Girma <96662906+nathnaelg@users.noreply.github.com>
2023-07-18 21:01:53 +0300

    Update 5-print_comb2.py

--- python-hello_world/5-print_comb2.py
+++ python-hello_world/5-print_comb2.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-for i in range(100):
-    if i == 99:
-        print(" {:02d}".format (i))
-        break
-    print ("{:02d}".format (i), end=", ")
+for i in range(99):
+    print("{:02d}".format(i), end=", ")
+print("{:d}".format(99))

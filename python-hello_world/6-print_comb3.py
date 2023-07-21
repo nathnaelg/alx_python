ea0a34c69b890bd2676a6e26dcdd02dafbcd301e
Natnael Girma <96662906+nathnaelg@users.noreply.github.com>
2023-07-21 07:48:40 +0300

    Update 6-print_comb3.py

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-for digit1 in range(0, 10):
-    for digit2 in range(digit1 + 1, 10):
-        if digit1 == 8 and digit2 == 9:
-            print("{}{}".format(digit1, digit2))
+for first_digit in range(10):
+    for second_digit in range(first_digit + 1, 10):
+        if first_digit != 8 or second_digit != 9:
+            print("{:d}{:d}, ".format(first_digit, second_digit), end="")
         else:
-            print("{}{}".format(digit1, digit2), end=", ")
+            print("{:d}{:d}".format(first_digit, second_digit))

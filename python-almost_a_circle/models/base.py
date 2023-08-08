5def72aca3883f310aeba534302af6c5fc06ee24
Natnael Girma <96662906+nathnaelg@users.noreply.github.com>
2023-08-08 22:33:01 +0300

    Create base.py

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Defines Base class """
+
+
+class Base:
+    """Class Base.
+
+    Attributes:
+        id (int): Identity of each instance.
+    """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """Creates instances of Base.
+
+        Args:
+            id (int, optional): Identity of instance.
+        """
+        if id == None:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+        else:
+            self.id = id

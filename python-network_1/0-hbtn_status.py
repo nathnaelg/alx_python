d5eb9bee803c0cd3cf1fa8f63dbfe8117927b5e7
Natnael Girma <96662906+nathnaelg@users.noreply.github.com>
2023-08-09 22:18:27 +0300

    Update 0-hbtn_status.py

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,8 +1,6 @@
 import requests
-
 url = "https://alu-intranet.hbtn.io/status"
 response = requests.get(url)
-
 print("Body response:")
 print("    - type:", type(response.text))
 print("    - content:", response.text)

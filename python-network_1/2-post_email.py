0dd85d80ec47d3df6f2c9ac5e69a56c76ceb78b5
Natnael Girma <96662906+nathnaelg@users.noreply.github.com>
2023-08-09 22:26:58 +0300

    Update 2-post_email.py

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -7,7 +7,7 @@ import requests
 if __name__ == '__main__':
     """ script that takes in a URL and
         an email address, sends a POST request
-        to the passed URL with the email as
+        to the passed URL with the email as 
         a parameter
     """
     url = sys.argv[1]

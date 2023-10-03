d761aa873c0ed2c9792256f7be5f7c2347ba487b
Natnael Girma <96662906+nathnaelg@users.noreply.github.com>
2023-10-03 09:45:56 +0300

    Update 2-export_to_JSON.py

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -1,59 +1,24 @@
-"""Accessing a REST API for todo lists of employees"""
-import json
-import requests
-import sys
+#!/usr/bin/python3
+"""fetches information from JSONplaceholder API and exports to JSON"""
 
-
-def get_employee_info(employee_id):
-    # Fetch employee details
-    employee_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
-    response = requests.get(employee_url)
-    employee_data = response.json()
-
-    if response.status_code != 200:
-        print(f"Error: Unable to fetch employee data for ID {employee_id}")
-        return
-
-    user_id = employee_data["id"]
-    username = employee_data["username"]
-
-    # Fetch TODO list for the employee
-    todos_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos"
-    response = requests.get(todos_url)
-    todos_data = response.json()
-
-    if response.status_code != 200:
-        print(f"Error: Unable to fetch TODO list for ID {employee_id}")
-        return
-
-    # Create a dictionary to store tasks
-    tasks_dict = {user_id: []}
-
-    for task in todos_data:
-        task_dict = {
-            "task": task["title"],
-            "completed": task["completed"],
-            "username": username
-        }
-        tasks_dict[user_id].append(task_dict)
-
-    # Export data to JSON file
-    json_file_name = f"{user_id}.json"
-    with open(json_file_name, 'w') as json_file:
-        json.dump(tasks_dict, json_file, indent=4)
-
-    print(f"Data exported to {json_file_name}")
+from json import dump
+from requests import get
+from sys import argv
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python script.py <employee_id>")
-        sys.exit(1)
+    todo_url = "https://jsonplaceholder.typicode.com/user/{}/todos".format(
+        argv[1])
+    name_url = "https://jsonplaceholder.typicode.com/users/{}".format(argv[1])
+    todo_result = get(todo_url).json()
+    name_result = get(name_url).json()
 
-    try:
-        employee_id = int(sys.argv[1])
-    except ValueError:
-        print("Error: Employee ID must be an integer")
-        sys.exit(1)
+    todo_list = []
+    for todo in todo_result:
+        todo_dict = {}
+        todo_dict.update({ "USER_ID": [{"task": "TASK_TITLE", "completed": TASK_COMPLETED_STATUS, "username": "USERNAME"}, {
+            "task": "TASK_TITLE", "completed": TASK_COMPLETED_STATUS, "username": "USERNAME"}, ... ]})
+        todo_list.append(todo_dict)
 
-    get_employee_info(employee_id)
+    with open("USER_ID.json".format(argv[1]), 'w') as f:
+        dump({argv[1]: todo_list}, f)

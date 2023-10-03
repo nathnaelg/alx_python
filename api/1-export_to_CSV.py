bf5c41697f389a6675f2a41651699ca33f6ca8f3
Natnael Girma <96662906+nathnaelg@users.noreply.github.com>
2023-10-03 09:57:49 +0300

    Update 1-export_to_CSV.py

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,17 +1,40 @@
-"""Accessing a REST API for todo lists of employees"""
+#!/usr/bin/python3
 import csv
 import requests
 import sys
 
-if __name__ == "__main__":
-    user_id = sys.argv[1]
-    url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users/{}".format(user_id)).json()
-    username = user.get("username")
-    todos = requests.get(url + "todos", params={"userId": user_id}).json()
 
-    with open("{}.csv".format(user_id), "w", newline="\n") as csvfile:
-        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
-        [writer.writerow(
-            [user_id, username, t.get("completed"), t.get("title")]
-        ) for t in todos]
+def export_to_CSV(employee_id):
+    # The API requests and data extraction will remain the same as in the provided script.
+
+    # Get employee details
+    user_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
+    user_response = requests.get(user_url)
+    user_data = user_response.json()
+    employee_name = user_data["username"]
+
+    # Get TODO list for the employee
+    todos_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos"
+    todos_response = requests.get(todos_url)
+    todos_data = todos_response.json()
+
+    # Create or overwrite the CSV file
+    with open(f"{employee_id}.csv", "w", newline='') as file:
+        writer = csv.writer(file, quoting=csv.QUOTE_ALL)
+        for task in todos_data:
+            writer.writerow([employee_id, employee_name,
+                            task["completed"], task["title"]])
+
+    print(f"Data exported to {employee_id}.csv")
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python3 script_name.py EMPLOYEE_ID")
+        sys.exit(1)
+
+    try:
+        employee_id = int(sys.argv[1])
+        export_to_CSV(employee_id)
+    except ValueError:
+        print("Please provide a valid employee ID.")
